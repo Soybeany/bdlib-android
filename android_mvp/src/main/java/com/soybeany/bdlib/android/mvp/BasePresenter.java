5f1a2b041dcafe42e07591ac85ce941fcbfe9899
Soybeany <soybeany@qq.com>
2019-04-12 13:43:38 +0800

    [~] 优化BasePresenter的API设计

--- android_mvp/src/main/java/com/soybeany/bdlib/android/mvp/BasePresenter.java
+++ android_mvp/src/main/java/com/soybeany/bdlib/android/mvp/BasePresenter.java
@@ -7,39 +7,36 @@ import com.soybeany.bdlib.android.util.IObserver;
 import com.soybeany.bdlib.core.java8.function.Consumer;
 import com.soybeany.bdlib.core.util.thread.ObjectStorage;
 
-import java.util.LinkedList;
-
 /**
  * <br>Created by Soybeany on 2019/4/11.
  */
-public abstract class BasePresenter implements IObserver {
+public abstract class BasePresenter implements IObserver, IUidOwner {
     private final ObjectStorage mStorage = new ObjectStorage();
-    private final LinkedList<String> mKeys = new LinkedList<>();
 
     public BasePresenter(@NonNull IView... views) {
         for (IView view : views) {
-            String uid = view.getUid();
-            mStorage.put(uid, view);
-            mKeys.add(uid);
+            mStorage.put(IUidOwner.getKey(view.getClass(), view.getUid()), view);
         }
-    }
-
-    protected <T> boolean invokeMain(Consumer<T> consumer, Class<T> clazz) {
-        if (mKeys.isEmpty()) {
-            return false;
-        }
-        return invoke(mKeys.peekFirst(), consumer, clazz);
-    }
-
-    /**
-     * 在指定key
-     */
-    protected <T> boolean invoke(@NonNull String key, Consumer<T> consumer, Class<T> clazz) {
-        return mStorage.invoke(key, consumer, clazz);
     }
 
     @Override
     public void onDestroy(@NonNull LifecycleOwner owner) {
         mStorage.clear();
+    }
+
+    protected IView getView(Class<? extends IView> clazz) {
+        return getView(clazz, IView.DEFAULT_UID);
+    }
+
+    protected IView getView(Class<? extends IView> clazz, String uid) {
+        return (IView) mStorage.get(IUidOwner.getKey(clazz, uid));
+    }
+
+    protected <T extends IView> boolean invoke(Class<T> clazz, Consumer<T> consumer) {
+        return invoke(clazz, IView.DEFAULT_UID, consumer);
+    }
+
+    protected <T extends IView> boolean invoke(Class<T> clazz, String uid, Consumer<T> consumer) {
+        return mStorage.invoke(IUidOwner.getKey(clazz, uid), consumer, clazz);
     }
 }
--- android_mvp/src/main/java/com/soybeany/bdlib/android/mvp/IView.java
+++ android_mvp/src/main/java/com/soybeany/bdlib/android/mvp/IView.java
@@ -1,17 +1,8 @@
 package com.soybeany.bdlib.android.mvp;
 
-import com.soybeany.bdlib.android.util.BDContext;
-
 /**
  * <br>Created by Soybeany on 2019/4/11.
  */
-public interface IView {
-
-    /**
-     * 用于在{@link BasePresenter}中区分当前视图
-     */
-    default String getUid() {
-        return BDContext.getUID();
-    }
+public interface IView extends IUidOwner {
 
 }
