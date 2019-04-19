38a7995f48402fc8803b109cb6635404100c2b47
Soybeany <soybeany@qq.com>
2019-04-19 15:32:37 +0800

    [~] IPresenterView增加showToast方法

--- android_mvp/src/main/java/com/soybeany/bdlib/android/mvp/IPresenterView.java
+++ android_mvp/src/main/java/com/soybeany/bdlib/android/mvp/IPresenterView.java
@@ -1,7 +1,15 @@
 package com.soybeany.bdlib.android.mvp;
 
+import android.support.annotation.MainThread;
+
+import com.soybeany.bdlib.android.util.ToastUtils;
+
 /**
  * <br>Created by Soybeany on 2019/4/16.
  */
 public interface IPresenterView {
+    @MainThread
+    default void showToast(String msg) {
+        ToastUtils.show(msg);
+    }
 }
