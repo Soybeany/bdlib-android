e2579f674f311f74f82406e45f8663f7507ae20a
Soybeany <soybeany@qq.com>
2019-05-06 19:15:58 +0800

    [~] 修改DrawableStatusBarUtils的实现

--- android_util/src/main/java/com/soybeany/bdlib/android/util/style/DrawableStatusBarUtils.java
+++ android_util/src/main/java/com/soybeany/bdlib/android/util/style/DrawableStatusBarUtils.java
@@ -3,7 +3,6 @@ package com.soybeany.bdlib.android.util.style;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.util.TypedValue;
@@ -20,7 +19,7 @@ import static com.soybeany.bdlib.android.util.BDContext.getResources;
 public class DrawableStatusBarUtils {
 
     /**
-     * 全面应用，带有监听器监听每次布局的变化，需在{@link android.app.Activity#setContentView(int)}后调用
+     * 全面应用，带有监听器监听每次布局的变化，需在{@link Activity#setContentView(int)}后调用
      *
      * @param res 资源值，为null时则使用主题默认的设置
      */
@@ -29,7 +28,7 @@ public class DrawableStatusBarUtils {
     }
 
     /**
-     * 简单应用，需在{@link android.app.Activity#setContentView(int)}后调用
+     * 简单应用，需在{@link Activity#setContentView(int)}后调用
      */
     public static void simpleApply(@NonNull Activity activity, @DrawableRes Integer res) {
         innerApply(activity, res, false);
@@ -40,24 +39,27 @@ public class DrawableStatusBarUtils {
             Resources resources = getResources();
             View statusBarV = decorView.findViewById(resources.getIdentifier("statusBarBackground", "id", "android"));
             Optional.ofNullable(statusBarV).ifPresent(v -> {
-                Drawable drawable = null != res ? resources.getDrawable(res) : getDefaultDrawable(activity);
-                setBackground(v, drawable);
+                setBackground(v, activity, res);
                 if (needAddListener) {
-                    decorView.addOnLayoutChangeListener((view, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> setBackground(v, drawable));
+                    decorView.addOnLayoutChangeListener((view, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> setBackground(v, activity, res));
                 }
             });
         }));
     }
 
-    private static void setBackground(@NonNull View statusBarV, @NonNull Drawable drawable) {
-        if (drawable != statusBarV.getBackground()) {
-            statusBarV.setBackground(drawable);
+    /**
+     * 每次均创建，以免不能触发底色替换
+     */
+    private static void setBackground(@NonNull View statusBarV, @NonNull Activity activity, @DrawableRes Integer res) {
+        // 设置为指定资源
+        if (null != res) {
+            statusBarV.setBackgroundResource(res);
+            return;
         }
-    }
-
-    private static Drawable getDefaultDrawable(@NonNull Activity activity) {
+        // 设置为默认资源
         TypedValue typedValue = new TypedValue();
         activity.getTheme().resolveAttribute(R.attr.colorPrimaryDark, typedValue, true);
-        return new ColorDrawable(typedValue.data);
+        int data = typedValue.data;
+        statusBarV.setBackground(new ColorDrawable(data));
     }
 }
