bf05058bd8847ad07863a0ed4f8d044b62ea9054
Soybeany <soybeany@qq.com>
2019-07-10 13:29:49 +0800

    [~] UICallback增加工作线程的回调

--- android_web/src/main/java/com/soybeany/bdlib/android/web/UICallback.java
+++ android_web/src/main/java/com/soybeany/bdlib/android/web/UICallback.java
@@ -7,24 +7,30 @@ import com.soybeany.bdlib.web.okhttp.core.ICallback;
 import static com.soybeany.bdlib.android.util.BDContext.MAIN_HANDLER;
 
 /**
+ * 使用UI线程的回调(也包含工作线程)
  * <br>Created by Soybeany on 2019/3/7.
  */
 public interface UICallback<Result> extends ICallback<Result> {
 
     @Override
     default void onSuccess(Result result) {
+        onWorkSuccess(result);
         MAIN_HANDLER.post(() -> onUISuccess(result));
     }
 
     @Override
     default void onFailure(boolean isCanceled, String msg) {
+        onWorkFailure(isCanceled, msg);
         MAIN_HANDLER.post(() -> onUIFailure(isCanceled, msg));
     }
 
     @Override
     default void onFinal(boolean isCanceled) {
+        onWorkFinal(isCanceled);
         MAIN_HANDLER.post(() -> onUIFinal(isCanceled));
     }
+
+    // //////////////////////////////////UI线程//////////////////////////////////
 
     /**
      * 成功时的回调(UI线程)
@@ -43,6 +49,28 @@ public interface UICallback<Result> extends ICallback<Result> {
      */
     default void onUIFinal(boolean isCanceled) {
     }
+
+    // //////////////////////////////////工作线程//////////////////////////////////
+
+    /**
+     * 成功时的回调(工作线程)
+     */
+    default void onWorkSuccess(Result result) {
+    }
+
+    /**
+     * 失败时的回调(工作线程)
+     */
+    default void onWorkFailure(boolean isCanceled, String msg) {
+    }
+
+    /**
+     * 最终的回调(工作线程)
+     */
+    default void onWorkFinal(boolean isCanceled) {
+    }
+
+    // //////////////////////////////////默认实现//////////////////////////////////
 
     interface Empty<Result> extends UICallback<Result> {
         @Override
