9875fae2e4b8d36db2774472ded170411c145c72
Soybeany <soybeany@qq.com>
2019-07-11 14:19:52 +0800

    [~] KeyboardUtils open方法放宽为View类型参数

--- android_util/src/main/java/com/soybeany/bdlib/android/util/system/KeyboardUtils.java
+++ android_util/src/main/java/com/soybeany/bdlib/android/util/system/KeyboardUtils.java
@@ -2,8 +2,8 @@ package com.soybeany.bdlib.android.util.system;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 
 /**
  * 软键盘工具类
@@ -21,24 +21,24 @@ public class KeyboardUtils {
     /**
      * 打开软键盘
      *
-     * @param context   上下文
-     * @param mEditText 输入框
+     * @param context 上下文
+     * @param view    视图
      */
-    public static void openKeyboard(Context context, EditText mEditText) {
+    public static void openKeyboard(Context context, View view) {
         InputMethodManager imm = getInputMethodManager(context);
-        imm.showSoftInput(mEditText, InputMethodManager.RESULT_SHOWN);
+        imm.showSoftInput(view, InputMethodManager.RESULT_SHOWN);
         imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 
     /**
      * 关闭软键盘
      *
-     * @param context   上下文
-     * @param mEditText 输入框
+     * @param context 上下文
+     * @param view    视图
      */
-    public static void closeKeyboard(Context context, EditText mEditText) {
+    public static void closeKeyboard(Context context, View view) {
         InputMethodManager imm = getInputMethodManager(context);
-        imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     /**
